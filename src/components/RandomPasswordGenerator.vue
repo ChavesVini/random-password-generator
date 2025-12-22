aa9057d87ae37d1521f2f2bf1afdc33ba79e2422
ChavesVini <vinidasilva2004@outlook.com>
2025-12-22 16:36:26 -0300

    :construction: generate random password logic completed

---
+++ src/components/RandomPasswordGenerator.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+const UPPER_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const LOWER_CASE_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+const NUMBERS = "0123456789"
+const SYMBOLS = "!\"#$%&'()*+-./:;<=>?@[\]^_`{|}~"
+
+function randomChar(chars: string): string {
+  return chars[Math.floor(Math.random() * chars.length)]
+}
+
+function shuffle(array: string[]): string[] {
+  return array.sort(() => Math.random() - 0.5)
+}
+
+function generate_random_password(upper: boolean, lower: boolean, number: boolean, symbols: boolean, password_length: number) {
+  let charPool = ""
+  let password: string[] = []
+
+  if (upper) charPool += UPPER_CASE_LETTERS
+  if (lower) charPool += LOWER_CASE_LETTERS
+  if (number) charPool += NUMBERS
+  if (symbols) charPool += SYMBOLS
+
+  while (password.length < password_length) {
+    password.push(randomChar(charPool))
+  }
+
+  return shuffle(password).join("")
+}
+</script>
+
+<template>
+
+</template>
---
+++ src/views/RandomPasswordGeneratorView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import RandomPasswordGenerator from '../components/RandomPasswordGenerator.vue'
+</script>
+
+<template>
+  <main>
+    <RandomPasswordGenerator />
+  </main>
+</template>
